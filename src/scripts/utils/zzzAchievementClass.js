6dcdcd1d67126c70d24d45b64268d1145d377fa8
ShawnSjl <sjlshawn@gmail.com>
2026-06-17 12:24:42 -0400

    feat: add new achievement class for city classes

--- src/scripts/utils/zzzAchievementClass.js
+++ src/scripts/utils/zzzAchievementClass.js
@@ -1,7 +1,7 @@
 export const categories = ['故事', '城市', '战术', '探索'];
 
 export const storyClasses = ['法厄同纪事', '代理人秘闻', '独家视界', '代理人信赖', '际会之时', '绳网热议']
-export const cityClasses = ['绳匠业务', '雅努斯区', '卫非地', '外环地带', '索恩区'];
+export const cityClasses = ['绳匠业务', '雅努斯区', '卫非地', '外环地带', '索恩区', '布亚斯特'];
 export const tacticsClasses = ['迷失之地', '枯萎之都', '作战技巧', '敌对目标', '战斗成就']
 export const explorationClasses = ['空洞探索指南', '零号密钥', '勘域探境']
 
